1556d1f244b66de4080cc527bd67db827455b4fa
SSDMT_DEV-HARLEY <51787264+SSDMT-DEV-HARLEY@users.noreply.github.com>
2021-03-17 00:25:51 +0800

    Add files via upload

---
+++ Discord One Click Bot Hosting.bat
@@ -0,0 +1,36 @@
+@ECHO OFF 
+color 0C
+ECHO Discord One Click Start Module V 0.1 (Final)
+ECHO ===========================
+ECHO System Now Initializing DOCBH and DOCSH Please dont panic if two Node Console appears on screen.
+ECHO This means the bot and status has Started succesfully.
+ECHO ==========================
+ECHO Discord One Click Bot Hosting
+ECHO ==========================
+ECHO File Found! (Index.js)
+cd C:\Users\ftaut\DoggoBot
+ECHO Initializing....
+ECHO ============================
+START Node index.js
+ECHO Service Started Succesfully.
+ECHO ============================
+ECHO Discord One Click Status Hosting
+ECHO ============================
+ECHO File Found! (Index.js)
+cd C:\Users\ftaut\Discord-Rich-Presence
+ECHO Initializing....
+ECHO ============================
+START Node index.js
+ECHO Service Started Succesfully.
+ECHO ============================
+ECHO Hey Join my server! 
+ECHO https://discord.gg/QFxHNZmRUD
+ECHO =============================
+ECHO ---------Hotdogs-------------
+ECHO =============================
+ECHO Join the Blob Development!
+ECHO https://discord.gg/FX3KhNjWtw
+ECHO =============================
+
+ECHO Thanks For Running me - Cafe Bot, Harley, Blob Development community
+PAUSE
